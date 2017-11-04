1fdbcbc21eeeaac6ed01c472c5522c5f6476d516
“Tea <teateearu@gmail.com>
2017-11-04 16:20:09 +0100

    Edit, delete, or create new closets

---
+++ app/controllers/closets_controller.rb
@@ -0,0 +1,48 @@
+class ClosetsController < ApplicationController
+  def index
+    @closets = Closet.all
+  end
+  #
+  def show
+    @closet = Closet.find(params[:id])
+  end
+  # def index
+  #   @closets = Closet.find(params[:id])
+  # end
+
+  def edit
+    @closet = Closet.find(params[:id])
+  end
+
+  def create
+      closet_params = params.require(:closet).permit(:name, :image_url)
+
+      @closet = Closet.new(closet_params)
+
+      if @closet.save
+         redirect_to @closet
+      else
+         render 'new'
+      end
+    end
+
+  def new
+   @closet = Closet.new
+  end
+
+  def update
+   @closet = Closet.find(params[:id])
+   closet_params = params.require(:closet).permit(:name, :image_url)
+   if @closet.update_attributes(closet_params)
+     redirect_to @closet
+   else
+     render 'edit'
+   end
+ end
+   def destroy
+     @closet = Closet.find(params[:id])
+     @closet.destroy
+     redirect_to closets_path
+   end
+
+end
