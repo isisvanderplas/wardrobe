0115cba5f407ae0be6fa650e96456982d4b16bbd
laraanna <lara@laraannawagner.com>
2017-11-04 14:22:06 +0100

    Create show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "clothes_items#index"
+
+  get "clothes_items" => "closets#index"
+
+  get "clothes_items/:id" => "clothes_items#show", as: :clothes_item
 end
